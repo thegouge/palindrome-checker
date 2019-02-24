b2245c119f13632bed70cafcccc4ad561bc1c883
thegouge <thegouge42@gmail.com>
2019-02-24 15:22:38 -0800

    cleaned it a bit

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ const output = document.getElementById("output");
 const submit = document.getElementById("submit");
 
 submit.addEventListener("click", () => {
-  let result = palindrome(input.value);
-
   output.innerHTML =
     input.value == ""
       ? "Empty input!"
-      : result
-        ? `The string ${input.value} is a palindrome!`
-        : `"${input.value}" is not a palindrome!`;
+      : palindrome(input.value)
+      ? `The string ${input.value} is a palindrome!`
+      : `"${input.value}" is not a palindrome!`;
 });
